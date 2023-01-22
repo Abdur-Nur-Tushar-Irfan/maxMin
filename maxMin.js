76b1bb71c6f936121861e50fa3144c387b37c713
Abdur Nur Tushar Irfan <tusharctg1202@gmail.com>
2023-01-22 13:05:54 +0600

    first commit

---
+++ maxMin.js
@@ -0,0 +1,10 @@
+let array = [10,1000,1,59,10,120,4,-1,30]
+function arrayMax(arr) {
+  var len = arr.length, max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
+    }
+  }
+  return max;
+};
